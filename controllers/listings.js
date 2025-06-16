9203e1a6267a50957841bec38ec52c8aec420902
saadkhan29 <sunny_khan_29@yahoo.com>
2025-06-16 20:15:44 +0300

    Open House Checkpoint 01

---
+++ controllers/listings.js
@@ -0,0 +1,69 @@
+const router = require('express').Router()
+
+const Listing = require('../models/listing');
+
+// API's/ Routes/ Main Functionality
+
+router.get('/', async(req, res) => {
+  const listings = await Listing.find().populate('owner');
+  res.render('listings/index.ejs', { listings });
+});
+
+router.get('/new', async (req, res) => {
+  res.render('listings/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+  req.body.owner = req.session.user._id;
+  await Listing.create(req.body);
+  res.redirect('/listings');
+});
+
+router.get("/:listingId", async (req, res) => {
+  const listing = await Listing.findById(req.params.listingId).populate('owner');
+  res.render('listings/show.ejs', { listing });
+});
+
+router.delete('/:listingId', async (req, res) => {
+  try {
+    const listing = await Listing.findById(req.params.listingId);
+    if (listing.owner.equals(req.session.user._id)) {
+      await listing.deleteOne();
+      res.redirect('/listings');
+    } else {
+      res.send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
+
+router.get('/:listingId/edit', async (req, res) => {
+  try {
+    const currentListing = await Listing.findById(req.params.listingId);
+    res.render('listings/edit.ejs', {
+      listing: currentListing,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.put('/:listingId', async (req, res) => {
+  try {
+    const currentListing = await Listing.findById(req.params.listingId);
+    if (currentListing.owner.equals(req.session.user._id)) {
+      await currentListing.updateOne(req.body);
+      res.redirect('/listings');
+    } else {
+      res.send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+module.exports = router;
